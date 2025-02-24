cefd580eaa6601620d506e49a17378ed935ba4c3
Marion-Anyango <anyangomarion515@gmail.com>
2025-02-24 11:19:25 +0300

    Created using Colab

---
+++ module.py
@@ -0,0 +1,67 @@
+{
+  "nbformat": 4,
+  "nbformat_minor": 0,
+  "metadata": {
+    "colab": {
+      "provenance": [],
+      "authorship_tag": "ABX9TyOqAB5km29Zzd+Ka44H5uop",
+      "include_colab_link": true
+    },
+    "kernelspec": {
+      "name": "python3",
+      "display_name": "Python 3"
+    },
+    "language_info": {
+      "name": "python"
+    }
+  },
+  "cells": [
+    {
+      "cell_type": "markdown",
+      "metadata": {
+        "id": "view-in-github",
+        "colab_type": "text"
+      },
+      "source": [
+        "<a href=\"https://colab.research.google.com/github/Marion-Anyango/binary-tree-/blob/main/module.py\" target=\"_parent\"><img src=\"https://colab.research.google.com/assets/colab-badge.svg\" alt=\"Open In Colab\"/></a>"
+      ]
+    },
+    {
+      "cell_type": "code",
+      "execution_count": 7,
+      "metadata": {
+        "id": "-ds7XW_E12d4"
+      },
+      "outputs": [],
+      "source": [
+        "x=[2,3,4,5,6,7,8]\n",
+        "y=[30,40,50,60,70,80]\n",
+        "a=20\n",
+        "b=30\n",
+        "def playnum (m,n):\n",
+        " while (m>0):\n",
+        "  print (m*n)\n",
+        "  m-=1\n",
+        "  return\n",
+        "  def donothing():\n",
+        "    print(\"doing nothing \")\n",
+        "    return\n",
+        "\n"
+      ]
+    },
+    {
+      "cell_type": "code",
+      "source": [
+        "  #with open('my_module.py', 'w') as f:\n",
+        "   # f.write('''def greet(name):\n",
+        "       # return f\"Hello, {name}!\"\n",
+        "   # ''')"
+      ],
+      "metadata": {
+        "id": "03OxWa2o7Gfx"
+      },
+      "execution_count": null,
+      "outputs": []
+    }
+  ]
+}
